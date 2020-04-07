6fecae72f7dd21586b549d83c8cdac6ab28a8aa1
Tor Kaufmann Gjerde <34443415+TorKG1@users.noreply.github.com>
2020-04-07 12:28:33 +0200

    Update oasis_main.py

--- oasis_main.py
+++ oasis_main.py
@@ -1,5 +1,12 @@
 
-from subprocess import Popen, PIPE
+# OASIS SW version 0.1
+# For use with parallel ADC board REV. 1 
+# a compiled C-code file "oasis_read_ADC" must be available in the same project folder. 
+
+# This script calls the lower level C code and returns a variable "outs" containing raw data
+# out from the ADS8422 ADC. Each 16-bit data sample is presented as 8-bit integer. 
+
+from subprocess import Popen, PIPE 
 import time
 
 counter = 0
