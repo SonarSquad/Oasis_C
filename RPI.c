b0772bfb3677652e9950af45f07ca32713efd1e5
Tor Kaufmann Gjerde <34443415+TorKG1@users.noreply.github.com>
2020-04-07 13:31:48 +0200

    Update RPI.c

--- RPI.c
+++ RPI.c
@@ -1,2 +1,10 @@
 
-#include "RPI.h"
+// Need go gain access to the memory of the BCM2835 in order to set specific registers that 
+// in turn is used to interface with the GPIOs 
+
+// the header file contains C declarations and macros definitions.
+// Macros a fragment of code which has been given a name.
+// wheneer the name is used, it is replaced by the contents of the macro. 
+
+#include "RPI.h
+
